befa6ac8475027f845db5e90b48183ad6202a8a0
Jirffey Gabuya <boyhimas@gmail.com>
2018-09-26 23:40:37 +0800

    Action types

---
+++ src/store/actions/types.js
@@ -0,0 +1,5 @@
+export const FETCH_LINKS = 'FETCH_LINKS';
+export const POST_LINK = 'POST_LINK';
+export const DELETE_LINK = 'DELETE_LINK';
+export const LIKE_LINK = 'LIKE_LINK';
+export const UNLIKE_LINK = 'UNLIKE_LINK';
