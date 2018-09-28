abf6a5e18031ba8927b34624ebcab54d584fd84a
Jirffey Gabuya <boyhimas@gmail.com>
2018-09-29 01:28:19 +0800

    Formatting

--- src/components/LinkForm.js
+++ src/components/LinkForm.js
@@ -2,33 +2,33 @@ import React from "react";
 import { Form, FormGroup, Label, Input, Button } from "reactstrap";
 import PropTypes from "prop-types";
 
-const propTypes = {	
-	onUrlChange: PropTypes.func.isRequired,
-	onSubmit: PropTypes.func.isRequired
+const propTypes = {
+    onUrlChange: PropTypes.func.isRequired,
+    onSubmit: PropTypes.func.isRequired
 };
 
 const LinkForm = props => {
-	return (
-		<div>
-			<Form onSubmit={props.onSubmit}>
-				<h4>New Link</h4>
+    return (
+        <div>
+            <Form onSubmit={props.onSubmit}>
+                <h4>New Link</h4>
 
-				<FormGroup>
-					<Label for="input-title">URL</Label>
-					<Input
-						type="text"
-						id="input-title"
+                <FormGroup>
+                    <Label for="input-title">URL</Label>
+                    <Input
+                        type="text"
+                        id="input-title"
                         onChange={props.onUrlChange}
                         required
-					/>
-				</FormGroup>
+                    />
+                </FormGroup>
 
-				<FormGroup>
-					<Button color="primary">Submit</Button>
-				</FormGroup>
-			</Form>
-		</div>
-	);
+                <FormGroup>
+                    <Button color="primary">Submit</Button>
+                </FormGroup>
+            </Form>
+        </div>
+    );
 };
 
 LinkForm.propTypes = propTypes;
--- src/containers/LinkFormContainer.js
+++ src/containers/LinkFormContainer.js
@@ -1,28 +1,28 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import LinkForm from '../components/LinkForm';
-import postLink from '../store/actions/post-link';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import LinkForm from "../components/LinkForm";
+import postLink from "../store/actions/post-link";
 
 class LinkFormContainer extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            url: '' 
-        }
+            url: ""
+        };
 
         this.onUrlChange = this.onUrlChange.bind(this);
     }
 
     onUrlChange(e) {
-        this.setState({ url: e.target.value })
-    }    
+        this.setState({ url: e.target.value });
+    }
 
     render() {
         return (
             <LinkForm
                 onUrlChange={this.onUrlChange}
-                onSubmit={(e) => {
+                onSubmit={e => {
                     e.preventDefault();
                     this.props.onSubmit(this.state);
                 }}
@@ -34,7 +34,10 @@ class LinkFormContainer extends Component {
 const mapStateToProps = state => state;
 
 const mapDispatchToProps = {
-	onSubmit: postLink
+    onSubmit: postLink
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(LinkFormContainer);
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(LinkFormContainer);
