03072eb989a0e1e30dd8f42ec755a82b9a67e8d6
Jirffey Gabuya <boyhimas@gmail.com>
2018-09-29 01:27:50 +0800

    Refactor
    - Change Links to LinkGroup
    - Change to flat heirarchy

---
+++ src/components/LinkGroup.js
@@ -0,0 +1,75 @@
+import React from "react";
+import PropTypes from "prop-types";
+import LinkItem from "./LinkItem";
+
+import moment from "moment";
+import { confirmAlert } from "react-confirm-alert";
+import "react-confirm-alert/src/react-confirm-alert.css";
+
+const propTypes = {
+    linkGroup: PropTypes.object.isRequired,
+    onDelete: PropTypes.func.isRequired,
+    onLike: PropTypes.func.isRequired,
+    onUnlike: PropTypes.func.isRequired
+};
+
+const renderLinks = (links, onLike, onUnlike, onDelete) => {
+    return links.map((item, index) => {
+        return (
+            <LinkItem
+                key={index}
+                id={item.id}
+                title={item.title}
+                description={item.description}
+                url={item.url}
+                imageUrl={item.image_url}
+                createdAt={item.created_at}
+                liked={item.liked}
+                likeCount={item.like_count}
+                owned={item.owned}
+                onLike={() => {
+                    onLike(item.id);
+                }}
+                onUnlike={() => {
+                    onUnlike(item.id);
+                }}
+                onDelete={() => {
+                    confirmAlert({
+                        title: "Confirm delete",
+                        message: "Are you sure?",
+                        buttons: [
+                            {
+                                label: "Yes",
+                                onClick: () => onDelete(item.id)
+                            },
+                            {
+                                label: "No"
+                            }
+                        ]
+                    });
+                }}
+            />
+        );
+    });
+};
+
+const LinkGroup = props => {
+    return (
+        <div>
+            <h4 className="mb-3 text-muted">
+                {moment.unix(props.linkGroup.timestamp).format("MMMM D, YYYY")}
+            </h4>
+
+            {renderLinks(
+                props.linkGroup.links,
+                props.onLike,
+                props.onUnlike,
+                props.onDelete
+            )}
+        </div>
+    );
+};
+
+LinkGroup.propTypes = propTypes;
+
+export default LinkGroup;
--- src/containers/LinksContainer.js
+++ src/containers/LinksContainer.js
@@ -1,55 +1,55 @@
 import React, { Component } from "react";
-import { Links } from "../components";
+import LinkGroup from "../components/LinkGroup";
 
 import { connect } from "react-redux";
-import moment from "moment";
+
 import fetchLinks from "../store/actions/fetch-links";
 import deleteLink from "../store/actions/delete-link";
 import likeLink from "../store/actions/like-link";
 import unlikeLink from "../store/actions/unlike-link";
+import { groupLinksByDayTimestamp } from "../util/data-operations";
 
 class LinksContainer extends Component {
-	componentDidMount() {
-        this.props.fetchLinks();        
-    }
-    
-    groupLinksByDate(links) {
-        return links.reduce((obj, curr) => {
-            const key = moment(moment(curr.created_at).format('MMMM D, YYYY')).unix();
-
-            if (typeof obj[key.toString()] === 'undefined') {
-                obj[key.toString()] = [];
-			}
-            
-            obj[key.toString()].push()
-
-            return obj;
-        }, {});
+    componentDidMount() {
+        this.props.fetchLinks();
     }
 
-	render() {
-		return (
-			<Links
-				links={this.props.links}
-                onDelete={this.props.onDelete}
-                onLike={this.props.onLike}
-                onUnlike={this.props.onUnlike}
-			/>
-		);
-	}
+    renderLinkGroups(linkGroups) {
+        return linkGroups.map((item, index) => {
+            return (
+                <LinkGroup
+                    className="mb-5"
+                    key={index}
+                    linkGroup={item}
+                    onDelete={this.props.onDelete}
+                    onLike={this.props.onLike}
+                    onUnlike={this.props.onUnlike}
+                />
+            );
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                {this.renderLinkGroups(
+                    groupLinksByDayTimestamp(this.props.links)
+                )}
+            </div>
+        );
+    }
 }
 
 const mapStateToProps = state => state;
 
 const mapDispatchToProps = {
-	fetchLinks: fetchLinks,
-	onDelete: deleteLink,
+    fetchLinks: fetchLinks,
+    onDelete: deleteLink,
     onLike: likeLink,
     onUnlike: unlikeLink
 };
 
 export default connect(
-	mapStateToProps,
-	mapDispatchToProps
+    mapStateToProps,
+    mapDispatchToProps
 )(LinksContainer);
-
