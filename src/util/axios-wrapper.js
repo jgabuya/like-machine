e8566aabe3526d0c91d550942d9bed08347a182a
Jirffey Gabuya <boyhimas@gmail.com>
2018-09-29 23:58:15 +0800

    Auto update bearer token when user is logged in
    Note: due to bug  #16 we commented out line 14 of axios-wrapper.js as a temporary fix

--- src/util/axios-wrapper.js
+++ src/util/axios-wrapper.js
@@ -1,9 +1,18 @@
 import axios from 'axios';
+import store from '../store';
 
+axios.defaults.baseURL = "https://likemachine-api.nerdgeschoss.de";
 axios.defaults.headers.common = {
-    'Authorization': 'Bearer 00000000-0000-0000-0000-000000000000'
+    Authorization: "Bearer 00000000-0000-0000-0000-000000000000"
 };
 
-axios.defaults.baseURL = 'https://likemachine-api.nerdgeschoss.de';
+// use sessionId if user is logged in
+store.subscribe(() => {
+    const state = store.getState();
+
+    if (state.user) {
+        // axios.defaults.headers.common = { Authorization: `Bearer ${state.user.sessionId}` };
+    }
+});
 
 export default axios;
