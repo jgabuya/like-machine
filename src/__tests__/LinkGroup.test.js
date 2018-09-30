a99920757a73298547a787532b29c17121836d6e
Jirffey Gabuya <boyhimas@gmail.com>
2018-10-01 04:26:32 +0800

    Fix LinkGroup test cases

--- src/__tests__/LinkGroup.test.js
+++ src/__tests__/LinkGroup.test.js
@@ -6,17 +6,18 @@ import moment from 'moment';
 
 describe("LinkGroup", () => {
     const props = {
-        linkGroup: { timestamp: moment().unix(), links: [] },
+        linkGroup: { timestamp: moment('1970-01-01').unix(), links: [] },
         user: null,
         onDelete: jest.fn(),
         onLike: jest.fn(),
         onUnlike: jest.fn()
     };
 
-    const wrapper = shallow(<LinkGroup {...props} />);
+    const component = <LinkGroup {...props} />;
+    const wrapper = shallow(component);
 
     it("matches snapshot", () => {
-        const tree = renderer.create(<LinkGroup {...props} />).toJSON();
+        const tree = renderer.create(component).toJSON();
         expect(tree).toMatchSnapshot();
     });
 
